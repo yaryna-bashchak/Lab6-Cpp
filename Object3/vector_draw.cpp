13b77e8538d1d3e1a15c36ac097dd9c7b4f72767
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-12-26 23:08:46 +0200

    parse data from clipboard

--- Object3/vector_draw.cpp
+++ Object3/vector_draw.cpp
@@ -30,3 +30,32 @@ long VectorDraw::GetFromClipboard(HWND hWnd, long maxsize)
 	CloseClipboard();
 	return res;
 }
+
+void VectorDraw::ParseClipboard()
+{
+    myVector.clear();
+
+    if (dataFromClipboard)
+    {
+        string data = string(dataFromClipboard);
+        size_t pos1 = 0;
+        string sep1 = "\n";
+
+        while ((pos1 = data.find(sep1)) != string::npos)
+        {
+            string line = data.substr(0, pos1);
+            data.erase(0, pos1 + sep1.length());
+
+            if (line != "")
+            {
+                double value;
+                size_t pos2 = 0;
+                string sep2 = "\t";
+
+                pos2 = line.find(sep2);
+                value = stod(line.substr(pos2 + sep2.length(), line.length()));
+                myVector.push_back(value);
+            }
+        }
+    }
+}
--- Object3/vector_draw.h
+++ Object3/vector_draw.h
@@ -1,10 +1,16 @@
 #pragma once
 #include "framework.h"
+#include <vector>
+#include <string>
+
+using namespace std;
 
 class VectorDraw
 {
 private:
 	char dataFromClipboard[1024];
+	vector<double> myVector;
 public:
 	long GetFromClipboard(HWND hWnd, long maxsize);
+	void ParseClipboard();
 };
